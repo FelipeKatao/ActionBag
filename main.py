12e5fe217a56d0ba2b98ec343508178d3b577d9c
Felipe <cataofelipe@gmail.com>
2022-04-05 02:41:45 -0300

    Numpy

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-
+import pandas
+import numpy as np
+import pandas as pd
 def teste(a):
     a = a+2;
     return a
@@ -10,3 +12,7 @@ def lol():
     pass
 
 print(teste(12))
+
+
+s = pd.Series(([1,3,5,np.nan,6,8]))
+print(s)
