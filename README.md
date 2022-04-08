479446c7931cddc77d1afd72ebb849469fe4d687
Felipe <cataofelipe@gmail.com>
2022-04-08 02:05:47 -0300

    criação da classe Actions

--- README.md
+++ README.md
@@ -6,3 +6,4 @@ https://www.b3.com.br/data/files/2B/41/CC/5D/10F42610D290A226790D8AA8/APIs-B3-Vi
 
 https://www.youtube.com/watch?v=FmEmlnvBcBw
 https://www.youtube.com/watch?v=RPfZBM3_jyk
+https://www3.bcb.gov.br/sgspub/localizarseries/localizarSeries.do?method=prepararTelaLocalizarSeries
--- actions.py
+++ actions.py
@@ -1,4 +1,5 @@
 import yfinance as yf
+import pandas as pd
 
 lmt = yf.Ticker("PETR4.SA")  #Tickets da entidade que se deseja buscar
 
@@ -7,3 +8,11 @@ hist = lmt.history(period="2022") #Data de agora
 #ist = lmt.history(period="1mo")  # Data Referente a um mês
 
 print(hist)
+
+def CdiConsultData(CdiData,valoresARecuperar):
+  # url = 'http://api.bcb.gov.br/dados/serie/bcdata.sgs.{}/dados?formato=json'.format(CdiData)
+   url = 'http://api.bcb.gov.br/dados/serie/bcdata.sgs.{}/dados/ultimos/{}?formato=json'.format(CdiData,valoresARecuperar)
+   df = pd.read_json(url)
+   print(df)
+
+CdiConsultData(12,2)
