49cee22e66978aaaad491d69a0adb5734399ad99
Felipe <cataofelipe@gmail.com>
2022-04-11 01:08:02 -0300

    update Actions.py

--- actions.py
+++ actions.py
@@ -1,18 +1,17 @@
 import yfinance as yf
 import pandas as pd
 
-lmt = yf.Ticker("PETR4.SA")  #Tickets da entidade que se deseja buscar
-
-#lmt.info
-hist = lmt.history(period="2022") #Data de agora 
-#ist = lmt.history(period="1mo")  # Data Referente a um mês
-
-#print(hist)
-
 def CdiConsultData(CdiData,valoresARecuperar):
   # url = 'http://api.bcb.gov.br/dados/serie/bcdata.sgs.{}/dados?formato=json'.format(CdiData)
    url = 'http://api.bcb.gov.br/dados/serie/bcdata.sgs.{}/dados/ultimos/{}?formato=json'.format(CdiData,valoresARecuperar)
    df = pd.read_json(url)
    print(df)
+   pass
+def ConsultarIndice(indice,periodo):
+  lmt = yf.Ticker(indice)
+  hist = lmt.history(period=periodo)
+  return hist
+  pass
 
 CdiConsultData(12,2)
+print(ConsultarIndice("PETR4.SA","1mo"))
