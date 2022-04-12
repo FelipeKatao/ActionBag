6a9c76d75c5437b2ee57154df1a70e4466850d83
Felipe <cataofelipe@gmail.com>
2022-04-12 01:19:42 -0300

    install flask

--- main.py
+++ main.py
@@ -3,10 +3,19 @@ import pandas
 import numpy as np
 import pandas as pd
 import actions as ac
+from flask import Flask, render_template, request
 
+app = Flask(__name__)
+
+@app.route('/',methods=["GET","POST"])
 def main():
-    ConsultarIndice = ac
-    print(ConsultarIndice.ConsultarIndice("PETR4.SA","1"))
-    pass
+    variavel =  "ticket"
+    return render_template("index.html", variavel = variavel)
 
-main()
+@app.route('/<string:nome>')
+def error(nome):
+    variavel = f'Pagina ({nome}) não existe'
+    return render_template("error.html", variavel = variavel)
+
+if __name__ == "__main__":
+    app.run();
