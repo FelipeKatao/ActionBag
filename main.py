791d40beb2e2a3fb0a588b0ba60a1a2e091315b4
Felipe <cataofelipe@gmail.com>
2022-04-13 23:36:44 -0300

    update main page

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ def main():
 
 @app.route('/acao/<string:ticket>')       
 def ticketload(ticket):
-    ticketSet = ticket
+    ticketSet = ticket  
     ticketname = ticket
     ticketnameSet = ticketname 
     ticketRead = ac.ConsultarIndice(ticket,'1y')
@@ -23,11 +23,11 @@ def ticketload(ticket):
 
 @app.route('/acao/<string:ticket>/<int:year>')       
 def ticketloadPeriod(ticket,periodo):
-    ticketRead = ac.ConsultarIndice(ticket,periodo)
+    ticketRead = ac.ConsultarIndice(ticket,periodo)  
     return render_template("tickets.html",ticketSet = periodo)
 
 @app.route('/<string:nome>')
-def error(nome):       
+def error(nome):        
     variavel =nome
     return render_template("error.html", variavel = variavel)  
 
